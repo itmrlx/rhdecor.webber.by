5bc7c70ff2951746c2b9877840f325d5fe53084c
itmrlx <itmrlx@gmail.com>
2016-10-15 14:43:36 +0300

    init
    pp main page

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/png" href="img/favicon-160.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon-114.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon-72.png">
+	<link rel="apple-touch-icon-precomposed" href="img/favicon-57.png">
+	<title>wedding</title>
+	<link rel="stylesheet" href="css/global.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<!-- header -->
+	<header class="wrapper header-t">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-2 left-container">
+					<div class="text">
+						<a href="/" class="logo">
+							<img src="img/logo.png" alt="logo">
+						</a>
+						<div class="burger-menu">
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+						<ul class="pro-menu">
+							<li class="current-menu-item"><a href="#">О нас</a></li>
+							<li><a href="#">Услуги</a></li>
+							<li><a href="#">Обучение</a></li>
+							<li><a href="#">Галерея работ</a></li>
+							<li><a href="#">Блог</a></li>
+							<li><a href="#">Контакты</a></li>
+						</ul>
+						<a href="#" class="btn btn-transparent">Получить звонок</a>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				<div class="col-xs-10 right-container">
+					<div class="slider-top">
+						<div class="slide">
+							<div class="text">
+								<div class="title">Добро пожаловать</div>
+								<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Текст страна деревни раз текстов страну продолжил ее моей переписали!</p>
+							</div>
+						</div>
+						<div class="slide">
+							<div class="text">
+								<div class="title">Добро пожаловать</div>
+								<p>Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Текст страна деревни раз текстов страну продолжил ее моей переписали!</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<!-- about -->
+	<div class="wrapper about-wr">
+		<div class="container">
+			<div class="col-xs-6 left">
+				<img src="img/about-img.jpg" alt="about">
+				<div class="centered">
+					<a href="#" class="btn btn-circle">Узнать больше</a>
+				</div>
+			</div>
+			<div class="col-xs-5 col-xs-offset-1 list">
+				<p>Свадебный декор/ украшение зала</p>
+				<p>Выездная регистрация</p>
+				<p>Свадебная фотозона</p>
+				<p>Букет невесты</p>
+				<p>Прокат декора</p>
+				<p>Аренда платьев трансформеров</p>
+				<p>Организация свадеб/ координация</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- portfolio -->
+	<div class="container portfolio-container">
+		<div class="title">ПОРТФОЛИО</div>
+		<div class="portfolio-row">
+			<div class="pp p35"><a href="img/portfolio.jpg" style="background-image: url(img/portfolio.jpg);" class="img fancybox" data-fancybox-group="main-portfolio"></a></div>
+			<div class="pp p25"><a href="img/portfolio.jpg" style="background-image: url(img/portfolio.jpg);" class="img fancybox" data-fancybox-group="main-portfolio"></a></div>
+			<div class="pp p40"><a href="img/portfolio.jpg" style="background-image: url(img/portfolio.jpg);" class="img fancybox" data-fancybox-group="main-portfolio"></a></div>
+			<div class="pp p26"><a href="img/portfolio.jpg" style="background-image: url(img/portfolio.jpg);" class="img fancybox" data-fancybox-group="main-portfolio"></a></div>
+			<div class="pp p40"><a href="img/portfolio.jpg" style="background-image: url(img/portfolio.jpg);" class="img fancybox" data-fancybox-group="main-portfolio"></a></div>
+			<div class="pp p34"><a href="img/portfolio.jpg" style="background-image: url(img/portfolio.jpg);" class="img fancybox" data-fancybox-group="main-portfolio"></a></div>
+			<div class="pp p35"><a href="img/portfolio.jpg" style="background-image: url(img/portfolio.jpg);" class="img fancybox" data-fancybox-group="main-portfolio"></a></div>
+			<div class="pp p25"><a href="img/portfolio.jpg" style="background-image: url(img/portfolio.jpg);" class="img fancybox" data-fancybox-group="main-portfolio"></a></div>
+			<div class="pp p40"><a href="img/portfolio.jpg" style="background-image: url(img/portfolio.jpg);" class="img fancybox" data-fancybox-group="main-portfolio"></a></div>
+			<div class="clearfix"></div>
+		</div>
+		<div class="centered">
+			<a href="#" class="btn btn-circle">Смотреть всё</a>
+		</div>
+	</div>
+
+	<!-- seporator -->
+	<div class="container separator">
+		<div class="row">
+			<div class="col-xs-10 col-xs-offset-1">
+				<div class="sep"><div class="sepimg"></div></div>
+			</div>
+		</div>
+	</div>
+
+	<!-- why we -->
+	<div class="wrapper why-we-wr">
+		<div class="container">
+			<div class="title">Почему стоит выбрать нас?</div>
+			<div class="row">
+				<div class="col-xs-4">
+					<img src="img/wedding-cake.svg" alt="icon">
+					<div class="title-b">Lorem ipsum dolor sit amet.</div>
+					<div class="desc">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Своего запятой вскоре свой коварный языкового взобравшись выйти дорогу эта!</div>
+				</div>
+				<div class="col-xs-4">
+					<img src="img/wedding-cake.svg" alt="icon">
+					<div class="title-b">Lorem ipsum dolor sit amet.</div>
+					<div class="desc">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Своего запятой вскоре свой коварный языкового взобравшись выйти дорогу эта!</div>
+				</div>
+				<div class="col-xs-4">
+					<img src="img/wedding-cake.svg" alt="icon">
+					<div class="title-b">Lorem ipsum dolor sit amet.</div>
+					<div class="desc">Далеко-далеко за словесными горами в стране, гласных и согласных живут рыбные тексты. Своего запятой вскоре свой коварный языкового взобравшись выйти дорогу эта!</div>
+				</div>
+			</div>
+			<div class="centered">
+				<a href="#" class="btn btn-circle">Расчитать стоимость праздника</a>
+			</div>
+		</div>
+	</div>
+
+	<!-- seporator -->
+	<div class="container separator">
+		<div class="row">
+			<div class="col-xs-10 col-xs-offset-1">
+				<div class="sep"><div class="sepimg"></div></div>
+			</div>
+		</div>
+	</div>
+
+	<!-- our team -->
+	<div class="wrapper our-team">
+		<div class="container">
+			<div class="title">Наша команда</div>
+			<div class="row">
+				<div class="col-xs-3 team">
+					<img src="img/joli.jpg" alt="joli">
+					<div class="name">Екатерина Иванова</div>
+					<div class="dol">Технический директор</div>
+				</div>
+				<div class="col-xs-3 team">
+					<img src="img/joli.jpg" alt="joli">
+					<div class="name">Екатерина Иванова</div>
+					<div class="dol">Технический директор</div>
+				</div>
+				<div class="col-xs-3 team">
+					<img src="img/joli.jpg" alt="joli">
+					<div class="name">Екатерина Иванова</div>
+					<div class="dol">Технический директор</div>
+				</div>
+				<div class="col-xs-3 team">
+					<img src="img/joli.jpg" alt="joli">
+					<div class="name">Екатерина Иванова</div>
+					<div class="dol">Технический директор</div>
+				</div>
+				<div class="col-xs-3 team">
+					<img src="img/joli.jpg" alt="joli">
+					<div class="name">Екатерина Иванова</div>
+					<div class="dol">Технический директор</div>
+				</div>
+				<div class="col-xs-3 team">
+					<img src="img/joli.jpg" alt="joli">
+					<div class="name">Екатерина Иванова</div>
+					<div class="dol">Технический директор</div>
+				</div>
+				<div class="col-xs-3 team">
+					<img src="img/joli.jpg" alt="joli">
+					<div class="name">Екатерина Иванова</div>
+					<div class="dol">Технический директор</div>
+				</div>
+				<div class="col-xs-3 team">
+					<img src="img/joli.jpg" alt="joli">
+					<div class="name">Екатерина Иванова</div>
+					<div class="dol">Технический директор</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- footer -->
+	<footer class="footer-b">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-8 left">
+					<div class="title">Мы в социальных сетях</div>
+					<div class="desc">Подпишись и следи за новостями</div>
+					<div class="col-xs-6">
+						soc
+					</div>
+					<div class="col-xs-6">
+						soc
+					</div>
+				</div>
+				<div class="col-xs-4 right">
+					<div class="logo-container">
+						<a href="/" class="logo"><img src="img/logo.png" alt="lofo"></a>
+					</div>
+					<div class="contacts-container">
+						<div class="title-c">Наши контакты</div>
+						<div class="phones">
+							<a href="tel:+375 29 999-99-99">+375 29 999-99-99</a>
+							<a href="tel:+375 29 999-99-99">+375 29 999-99-99</a>
+						</div>
+						<a href="mailto:info@rhdecor.by" class="mail">info@rhdecor.by</a>
+					</div>
+					<div class="copyright">
+						<p class="webber">Разработка сайта<a href="http://webber.by"><img src="img/webber-gray.svg" alt="Webber создание сайтов">Webber Studio</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+	
+	<script src="js/min/jquery-3.0.0-min.js"></script>
+	<script src="js/min/jquery.fancybox-min.js"></script>
+	<script src="js/min/slick-min.js"></script>
+	<script src="js/min/script-min.js"></script>
+</body>
+</html>
